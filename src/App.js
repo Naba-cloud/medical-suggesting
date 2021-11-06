323a28818e8d1d53ad46c36b3e87ab925ce7ee9d
Nabacloud <nabazehra45@gmail.com>
2021-11-06 23:46:49 +0500

    signup login done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import React from "react"
 import Signup from './Signup'
+import Login from './Login';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Signup/>
+      {/* <Signup/> */}
+      <Login/>
       </div>
   );
 }
