136c2afc0b8bc86896bd7fc01276ae2f1e3e31da
Nabacloud <nabazehra45@gmail.com>
2021-11-08 23:11:50 +0500

    Fix

--- src/Components/Context.js
+++ src/Components/Context.js
@@ -1,5 +1,2 @@
-import React from 'react';
-const MyContext = React.createContext();
-const Signupcon = React.createContext();
-
-export { MyContext, Signupcon };
+const Context=createContext({});
+export default Context;
