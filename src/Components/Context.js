3e1c0a36ec352881e199aa6585c6b4d5f8e1e757
fahadaleem <faleem396@gmail.com>
2021-11-09 00:07:11 +0500

    Disease page created, disease api created.

--- src/Components/Context.js
+++ src/Components/Context.js
@@ -1,2 +1,14 @@
-const Context=createContext({});
-export default Context;
+import { createContext, useState } from "react";
+
+const AppContext = createContext();
+
+const AppContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  return (
+    <AppContext.Provider value={{ user, handleSetUser: setUser }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export { AppContextProvider, AppContext };
--- src/layout/routes.js
+++ src/layout/routes.js
@@ -2,14 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import Signup from "../Components/signup/Signup";
 import Login from "../Components/login/Login";
 import Disease from "../Components/diseases/Disease";
+import Patients from "../Components/Patients/Patients";
 
 const Layout = () => {
   return (
     <Routes>
       <Route exact path="/" element={<h1>hello</h1>} />
-      <Route path="/signup-doctor" element={<Signup />} /> 
-      <Route path="/login" element={<Login />} /> 
-      <Route path="/disease" element={<Disease />} /> 
+      <Route path="/signup-doctor" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/disease" element={<Disease />} />
+      <Route path="/patients" element={<Patients />} />
     </Routes>
   );
 };
