109ad1c862eb21c1bebc7d2acbf046c105be6e2b
fahadaleem <faleem396@gmail.com>
2021-11-10 19:03:28 +0500

    admin, patient dashboard created.

--- src/Components/Context.js
+++ src/Components/Context.js
@@ -5,6 +5,9 @@ const AppContext = createContext();
 const AppContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [allDoctors, setDoctors] = useState([]);
+  const [doctorDetails, setDoctorDetails] = useState({});
+  const [allPatients, setPatients] = useState([]);
+  const [patientDetails, setPatientDetails] = useState({});
   return (
     <AppContext.Provider
       value={{
@@ -12,6 +15,12 @@ const AppContextProvider = ({ children }) => {
         handleSetUser: setUser,
         allDoctors,
         handleSetDoctors: setDoctors,
+        doctorDetails,
+        handleSetDoctorDetails: setDoctorDetails,
+        allPatients,
+        handleSetPatients: setPatients,
+        patientDetails,
+        handleSetPatientDetails: setPatientDetails,
       }}
     >
       {children}
--- src/layout/routes.js
+++ src/layout/routes.js
@@ -4,6 +4,12 @@ import Login from "../Components/login/Login";
 import Disease from "../Components/diseases/Disease";
 import Patients from "../Components/Patients/Patients";
 import SpecialistDoctors from "../Components/SpecialistDoctors/SpecialistDoctos";
+import AdminLogin from "../Components/Admin/AdminLogin";
+import AdminDashboard from "../Components/Admin/AdminDashboard";
+import AllDoctors from "../Components/Admin/AllDoctors";
+import DoctorsDetails from "../Components/Admin/DoctorDetails";
+import AllPatients from "../Components/Admin/AllPatients";
+import PatientDetails from "../Components/Admin/PatientDetails";
 
 const Layout = () => {
   return (
@@ -14,6 +20,12 @@ const Layout = () => {
       <Route path="/disease" element={<Disease />} />
       <Route path="/patients" element={<Patients />} />
       <Route path="/specialist-doctors" element={<SpecialistDoctors />} />
+      <Route path="/admin" element={<AdminLogin />} />
+      <Route path="/admin-dashboard" element={<AdminDashboard />} />
+      <Route path="/all-doctors" element={<AllDoctors />} />
+      <Route path="/doctor-details" element={<DoctorsDetails />} />
+      <Route path="/all-patients" element={<AllPatients />} />
+      <Route path="/patient-details" element={<PatientDetails />} />
     </Routes>
   );
 };
